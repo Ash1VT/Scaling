96a8ed526043867af14d2467acca28d168bb1aa2
Ash1VT <nikita.panasevitch@yandex.by>
2022-09-26 22:46:43 +0300

    added more

---
+++ Scaling/ImageExtension.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Scaling
+{
+    public class ImageExtension
+    {
+        
+        private Color _emptyColor;
+        
+        
+        public ImageExtension(Color emptyColor)
+        {
+            // _oldBitmap = oldBitmap;
+            // _xCoefficient = xCoefficient;
+            // _yCoefficient = yCoefficient;
+            _emptyColor = emptyColor;
+            // InitNewBitmap();
+        }
+
+        public Bitmap Extend(Bitmap bitmap, double xCoefficient, double yCoefficient)
+        {
+            // size: 10 x 1 
+            // xcoef = 3.5
+            // ycoef = 2
+            // newSize: 20 x 1
+            // 1 2 1 1 1 1 1 1 1 1    1 0 2 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0
+            // 
+            // 1 2 6
+            // 3 4 7
+            //
+            //
+            // 1 0 0 2 0 0 0 6 0 0
+            // 0 0 0 0 0 0 0 0 0 0
+            // 3 0 0 4 0 0 0 7 0 0
+            // 0 0 0 0 0 0 0 0 0 0
+            //
+            // x = 1 / 3 = 0,33 , y = 0
+            // x' = 0, y' = 0
+            // b = 0
+            // a = 0,33
+            // I(x,y) = 1 * 1 * I1(1,0)
+            
+            Size newSize = SizeCalculator.Calculate(bitmap.Size, xCoefficient, yCoefficient);
+            Bitmap newBitmap = new Bitmap(newSize.Width, newSize.Height);
+            
+            FillWithEmptyColor(newBitmap);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    newBitmap.SetPixel((int)(x * xCoefficient), (int)(y * yCoefficient), bitmap.GetPixel(x, y));
+                }
+            }
+
+            return newBitmap;
+        }
+
+        private void FillWithEmptyColor(Bitmap bitmap)
+        {
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    bitmap.SetPixel(x,y, _emptyColor);
+                }
+            }
+        }
+    }
+}
---
+++ Scaling/Utility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Scaling
+{
+    public class Utility
+    {
+        // public static bool Compare(Color color1, Color color2)
+        // {
+        //     return color1.R == color2.R && color1.B == color2.B && color1.G == color2.G;
+        // }
+        public static List<double> MultiplyColorValues(List<double> color_values, double value)
+        {
+            List<double> new_color_values = new List<double>();
+            foreach (var colorValue in color_values)
+            {
+                new_color_values.Add(colorValue * value);
+            }
+
+            return new_color_values;
+        }
+
+        public static List<double> SumColorValues(List<double> color1_values, List<double> color2_values)
+        {
+
+            List<double> new_color_values = new List<double>();
+            for (int i = 0; i < color1_values.Count; i++)
+            {
+                new_color_values.Add(color1_values[i] + color2_values[i]);
+                
+            }
+
+            return new_color_values;
+        }
+
+        public static Color ConvertFromValues(List<double> color_values)
+        {
+            return Color.FromArgb((int)color_values[0], (int)color_values[1], (int)color_values[2]);
+        }
+        
+        public static List<double> ConvertFromColor(Color color)
+        {
+            List<double> color_values = new List<double>()
+            {
+                color.R,
+                color.G,
+                color.B
+            };
+            return color_values;
+        }
+        public static String BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+    }
+}
