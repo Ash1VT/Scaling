d8e06567442852ecef5146073d34d1ea5f6dbbdd
ked500 <ked500@tut.by>
2022-09-28 23:34:01 +0300

    ,

--- Scaling/SizeCalculator.cs
+++ Scaling/SizeCalculator.cs
@@ -32,5 +32,23 @@ namespace Scaling
             return new Size(newWidth, newHeight);
 
         }
+
+
+
+
+        // 15 30 15 
+        // 30 15 30
+        // y = 0
+        // x = 0 => 15;
+        // x + 1 => 30; min = 15; max = 30; (max - min) / k = 5 - op;
+        // (k - 1 time) i = 1 i < k -1; 
+        // min(15) + op(5) = 20
+        // prev(20) + op(5) = 25
+
+        // new List() - значения k - 1 раз
+        // list<list>.add(list)
+        // if x + 1 > x
+        // then 
+        // list<list>.sort(dec) else acs
     }
 }
